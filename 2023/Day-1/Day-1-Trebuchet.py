8260888929030ecbeb669282940d20c19fdded11
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2023-12-04 14:25:56 -0800

    Day 1 complete

--- 2023/Day-1/Day-1-Trebuchet.py
+++ 2023/Day-1/Day-1-Trebuchet.py
@@ -4,49 +4,24 @@
 
 # What is the sum of all the calibration values?
 
-# Define a dictionary that maps
-# numeric words to their corresponding digits
-word_to_digit = {
-    'zero': '0',
-    'one': '1',
-    'two': '2',
-    'three': '3',
-    'four': '4',
-    'five': '5',
-    'six': '6',
-    'seven': '7',
-    'eight': '8',
-    'nine': '9'
-}
-
-# function to take in file
 def process_data(infile):
+    # function to take in file
     with open(infile) as f:
         data = f.read().split("\n")
         return data
 
-# function to parse last digit and first digit to form a single 2-digit number
-def get_calibration_values(calibration_data):
-    
-    digits = []
-    current_digit = ''
+def sum_calibration_values(calibration_data):
+    results_sum = 0
 
     for value in calibration_data:
-        for char in value:
-            # check if char is in the dictionary, if it is return value
-            if char.isdigit() and len(current_digit) != 2:
-                current_digit += char
-        digits.append(current_digit)
-    return digits
-
-
-# function to sum all the values and return it
-def sum_calibration_values():
-    pass
+        digits = [char for char in value if char.isdigit()]
+        results_sum += int(digits[0] + digits[-1])
+    return results_sum
 
 def main():
     data = process_data("calibration_doc.txt")
-    calibration_values = print(get_calibration_values(data))
+    calibration_values = sum_calibration_values(data)
+    print(calibration_values)
 
 if __name__ == '__main__':
     main()
